0d9bbbe01b54039ec71d188f89a5368670b1fe90
Brandon G-Home <brandon.gardner2@outlook.com>
2019-04-02 11:40:43 -0500

    Implemented session config

--- api/server.js
+++ api/server.js
@@ -1,20 +1,34 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require("express-session");
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
 
 const server = express();
+
+const sessionConfig = {
+  name: "monster",
+  secret: "keep it secret, keep it safe!",
+  cookie: {
+    maxAge: 1000 * 60 * 10, //milliseconds (10 minutes)
+    secure: false, //use cookie over https if true
+    httpOnly: true //can JS access the cookie on the client?
+  },
+  resave: false, // avoid recreating unchanged sessions
+  saveUninitialized: false // GDPR compliance
+};
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send("It's alive!");
 });
 
